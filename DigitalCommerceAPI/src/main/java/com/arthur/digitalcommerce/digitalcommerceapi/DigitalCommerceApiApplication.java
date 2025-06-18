81bc8f11bd442b8623eda3117fa7340d8c4f723b
arthur-343 <arthurssace@gmail.com>
2025-06-18 13:25:40 -0300

    Adding local project files

---
+++ DigitalCommerceAPI/src/main/java/com/arthur/digitalcommerce/digitalcommerceapi/DigitalCommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.arthur.digitalcommerce.digitalcommerceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalCommerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalCommerceApiApplication.class, args);
+	}
+
+}
---
+++ DigitalCommerceAPI/src/test/java/com/arthur/digitalcommerce/digitalcommerceapi/DigitalCommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthur.digitalcommerce.digitalcommerceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalCommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
